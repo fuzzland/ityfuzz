9ad55687e8f8e3a47d42c30f84a7f150051c8189
shou <shou@berkeley.edu>
2022-10-26 08:43:03 -0700

    finish abi for array

--- src/abi.rs
+++ src/abi.rs
@@ -5,6 +5,8 @@ use serde::{Deserialize, Serialize};
 // how can we deserialize this trait?
 pub trait ABI: CloneABI + serde_traitobject::Serialize + serde_traitobject::Deserialize {
     fn set_bytes(&mut self);
+    fn is_static(&self) -> bool;
+    fn get_bytes(&self) -> &Bytes;
 }
 
 trait CloneABI {
@@ -36,6 +38,14 @@ impl BoxedABI {
     pub fn get(&self) -> &Box<dyn ABI> {
         &self.b
     }
+
+    pub fn get_bytes(&self) -> &Bytes {
+        self.b.get_bytes()
+    }
+
+    pub fn is_static(&self) -> bool {
+        self.b.is_static()
+    }
 }
 
 impl Clone for Box<dyn ABI> {
@@ -48,30 +58,24 @@ impl Clone for Box<dyn ABI> {
 #[derive(Clone, Serialize, Deserialize)]
 pub struct A256 {
     data: [u8; 32],
+    // data len is a constant after initialization
     data_len: usize,
-    bytes: Bytes,
+    pub bytes: Bytes,
 }
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct ADynamic {
     data: Vec<u8>,
     multiplier: usize,
-    bytes: Bytes,
-}
-
-#[derive(Clone, Serialize, Deserialize)]
-pub struct AFixedArray {
-    data: Vec<BoxedABI>,
-    data_len: usize,
-    dynamic: bool,
-    bytes: Bytes,
+    pub bytes: Bytes,
 }
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct AArray {
     data: Vec<BoxedABI>,
     data_len: usize,
-    bytes: Bytes,
+    dynamic_size: bool,
+    pub bytes: Bytes,
 }
 
 // FIXME: fix scope
@@ -85,21 +89,20 @@ fn pad_bytes(bytes: &mut Bytes, len: usize) {
 impl ABI for A256 {
     fn set_bytes(&mut self) {
         unsafe {
-            let mut counter = 0;
             let ptr = self.bytes.as_mut_ptr();
-            loop {
-                *ptr = if self.data_len < (32 - counter) {
-                    0
-                } else {
-                    self.data[counter - (32 - self.data_len)]
-                };
-                counter += 1;
-                if counter == 32 {
-                    break;
-                }
-                ptr = ptr.add(1);
+            ptr = ptr.add(32 - self.data_len);
+            for i in 0..self.data_len {
+                *ptr.add(i) = self.data[i];
             }
         }
+    }
+
+    fn is_static(&self) -> bool {
+        true
+    }
+
+    fn get_bytes(&self) -> &Bytes {
+        &self.bytes
     }
 }
 
@@ -130,28 +133,87 @@ impl ABI for ADynamic {
             }
         }
     }
-}
 
-// FIXME: fix scope
-impl ABI for AFixedArray {
-    fn set_bytes(&mut self) {
-        todo!()
+    fn is_static(&self) -> bool {
+        false
+    }
+
+    fn get_bytes(&self) -> &Bytes {
+        &self.bytes
     }
 }
 
 impl ABI for AArray {
     fn set_bytes(&mut self) {
-        todo!()
+        let mut tails: Vec<&Bytes> = Vec::new();
+        let mut tails_offset: Vec<usize> = Vec::new();
+        let mut head: Vec<&Bytes> = Vec::new();
+        let mut head_data: Vec<&Bytes> = Vec::new();
+        let mut head_size: usize = 0;
+        let dummy_bytes = Bytes::new();
+        for i in 0..self.data.len() {
+            if self.data[i].is_static() {
+                let encoded = self.data[i].get_bytes();
+                head.push(encoded);
+                tails.push(&dummy_bytes);
+                head_size += encoded.len();
+            } else {
+                tails.push(self.data[i].get_bytes());
+                head.push(&dummy_bytes);
+                head_size += 32;
+            }
+        }
+        let mut content_size: usize = 0;
+        tails_offset.push(0);
+        if tails.len() > 0 {
+            for i in 0..tails.len() - 1 {
+                content_size += tails[i].len();
+                tails_offset.push(content_size);
+            }
+            for i in 0..tails_offset.len() {
+                if head[i].len() == 0 {
+                    head_data.push(&Bytes::from(vec![0; 32]));
+                    set_size(head_data[i].as_mut_ptr(), tails_offset[i] + head_size);
+                } else {
+                    head_data.push(head[i]);
+                }
+            }
+        }
+        let mut bytes = Bytes::from(vec![
+            0;
+            head_data.into_iter().map(|x| x.len()).sum::<usize>()
+                + tails.into_iter().map(|x| x.len()).sum::<usize>()
+                + if self.dynamic_size { 32 } else { 0 }
+        ]);
+
+        if self.dynamic_size {
+            set_size(bytes.as_mut_ptr(), self.data.len());
+        }
+        let mut offset: usize = if self.dynamic_size { 32 } else { 0 };
+        for i in 0..head_data.len() {
+            bytes[offset..offset + head_data[i].len()].copy_from_slice(head_data[i]);
+            offset += head_data[i].len();
+        }
+        for i in 0..tails.len() {
+            bytes[offset..offset + tails[i].len()].copy_from_slice(tails[i]);
+            offset += tails[i].len();
+        }
+        self.bytes = bytes;
+    }
+
+    fn is_static(&self) -> bool {
+        if self.dynamic_size {
+            false
+        } else {
+            self.data.iter().all(|x| x.is_static())
+        }
+    }
+
+    fn get_bytes(&self) -> &Bytes {
+        &self.bytes
     }
 }
 
 // test serialization and deserialization
 #[test]
-fn test_abi() {
-    let a = A256 {
-        data: [0; 32],
-        data_len: 32,
-    };
-    let c = serde_json::to_string(&a).unwrap();
-    println!("{}", c);
-}
+fn test_abi() {}
