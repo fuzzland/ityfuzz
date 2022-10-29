ac41e76aaeded4284fe7897bc174f169ecc83d3f
shou <shou@berkeley.edu>
2022-10-28 18:08:51 -0700

    handle shadow stack without lifetime

--- src/concolic/concolic_host.rs
+++ src/concolic/concolic_host.rs
@@ -9,69 +9,240 @@ use revm::{
 };
 use std::borrow::{Borrow, BorrowMut};
 use std::collections::HashMap;
-use std::ops::{Add, Mul, Sub};
+use std::ops::{Add, Deref, Mul, Sub};
 use std::str::FromStr;
+use revm::opcode::CALL;
 use z3::ast::{Bool, BV};
 use z3::{ast, ast::Ast, Config, Context, Solver};
 
-pub struct ConcolicHost<'a> {
+#[derive(Clone)]
+enum ConcolicOp {
+    U256, ADD, DIV, MUL, SUB, SDIV, SMOD, UREM, SREM,
+    AND, OR, XOR, NOT, NONE,
+    SHL, SHR, SAR,
+    INPUT, SLICEDINPUT, BALANCE, CALLVALUE
+}
+
+pub enum ConstraintOp {
+    EQ, LT, SLT, GT, SGT
+}
+
+#[derive(Clone)]
+pub struct BVBox {
+    lhs: Option<Box<BVBox>>,
+    rhs: Option<Box<BVBox>>,
+    concrete: Option<U256>,
+    op: ConcolicOp
+}
+
+pub struct Constraint {
+    pub lhs: Box<BVBox>,
+    pub rhs: Box<BVBox>,
+    pub op: ConstraintOp
+}
+
+
+macro_rules! box_bv {
+    ($lhs:expr, $rhs:expr, $op:expr) => {
+        Box::new(BVBox {
+            lhs: Some(Box::new($lhs)),
+            rhs: Some($rhs),
+            concrete: None,
+            op: $op
+        })
+    };
+}
+impl BVBox {
+    pub fn new() -> Self {
+        BVBox {
+            lhs: None,
+            rhs: None,
+            concrete: None,
+            op: ConcolicOp::NONE
+        }
+    }
+
+    pub fn new_input() -> Self {
+        BVBox {
+            lhs: None,
+            rhs: None,
+            concrete: None,
+            op: ConcolicOp::INPUT
+        }
+    }
+
+
+    pub fn new_sliced_input(idx: U256) -> Self {
+        BVBox {
+            lhs: None,
+            rhs: None,
+            concrete: Some(idx),
+            op: ConcolicOp::SLICEDINPUT
+        }
+    }
+
+    pub fn new_balance() -> Self {
+        BVBox {
+            lhs: None,
+            rhs: None,
+            concrete: None,
+            op: ConcolicOp::BALANCE
+        }
+    }
+
+    pub fn new_callvalue() -> Self {
+        BVBox {
+            lhs: None,
+            rhs: None,
+            concrete: None,
+            op: ConcolicOp::CALLVALUE
+        }
+    }
+
+    pub fn div(self, rhs: Box<BVBox>) -> Box<BVBox> {
+    box_bv!(self, rhs, ConcolicOp::DIV)
+}
+    pub fn mul(self, rhs: Box<BVBox>) -> Box<BVBox> {
+        box_bv!(self, rhs, ConcolicOp::MUL)
+    }
+    pub fn add(self, rhs: Box<BVBox>) -> Box<BVBox> {
+        box_bv!(self, rhs, ConcolicOp::ADD)
+    }
+    pub fn sub(self, rhs: Box<BVBox>) -> Box<BVBox> {
+        box_bv!(self, rhs, ConcolicOp::SUB)
+    }
+    pub fn bvsdiv(self, rhs: Box<BVBox>) -> Box<BVBox> {
+        box_bv!(self, rhs, ConcolicOp::SDIV)
+    }
+    pub fn bvsmod(self, rhs: Box<BVBox>) -> Box<BVBox> {
+        box_bv!(self, rhs, ConcolicOp::SMOD)
+    }
+    pub fn bvurem(self, rhs: Box<BVBox>) -> Box<BVBox> {
+        box_bv!(self, rhs, ConcolicOp::UREM)
+    }
+    pub fn bvsrem(self, rhs: Box<BVBox>) -> Box<BVBox> {
+        box_bv!(self, rhs, ConcolicOp::SREM)
+    }
+    pub fn bvand(self, rhs: Box<BVBox>) -> Box<BVBox> {
+        box_bv!(self, rhs, ConcolicOp::ADD)
+    }
+    pub fn bvor(self, rhs: Box<BVBox>) -> Box<BVBox> {
+        box_bv!(self, rhs, ConcolicOp::OR)
+    }
+    pub fn bvxor(self, rhs: Box<BVBox>) -> Box<BVBox> {
+        box_bv!(self, rhs, ConcolicOp::XOR)
+    }
+    pub fn bvnot(self) -> Box<BVBox> {
+        box_bv!(self, Box::new(BVBox::new()), ConcolicOp::NOT)
+    }
+    pub fn bvshl(self, rhs: Box<BVBox>) -> Box<BVBox> {
+        box_bv!(self, rhs, ConcolicOp::SHL)
+    }
+    pub fn bvlshr(self, rhs: Box<BVBox>) -> Box<BVBox> {
+        box_bv!(self, rhs, ConcolicOp::SHR)
+    }
+    pub fn bvsar(self, rhs: Box<BVBox>) -> Box<BVBox> {
+        box_bv!(self, rhs, ConcolicOp::SAR)
+    }
+
+    pub fn bvult(self, rhs: Box<BVBox>) -> Constraint {
+        Constraint {
+            lhs: Box::new(self),
+            rhs,
+            op: ConstraintOp::LT
+        }
+    }
+    pub fn bvugt(self, rhs: Box<BVBox>) -> Constraint {
+        Constraint {
+            lhs: Box::new(self),
+            rhs,
+            op: ConstraintOp::GT
+        }
+    }
+
+    pub fn bvslt(self, rhs: Box<BVBox>) -> Constraint {
+        Constraint {
+            lhs: Box::new(self),
+            rhs,
+            op: ConstraintOp::SLT
+        }
+    }
+    pub fn bvsgt(self, rhs: Box<BVBox>) -> Constraint {
+        Constraint {
+            lhs: Box::new(self),
+            rhs,
+            op: ConstraintOp::SGT
+        }
+    }
+
+    pub fn eq(self, rhs: Box<BVBox>) -> Constraint {
+        Constraint {
+            lhs: Box::new(self),
+            rhs,
+            op: ConstraintOp::EQ
+        }
+    }
+
+
+}
+
+
+pub struct ConcolicHost {
     env: Env,
     data: HashMap<H160, HashMap<U256, U256>>,
     code: HashMap<H160, Bytecode>,
-    solver: Solver<'a>,
-    ctx: Context,
-    symbolic_stack: Vec<Option<BV<'a>>>,
-    shadow_inputs: Option<BV<'a>>,
+    // solver: Solver<'a>,
+    // ctx: &'static mut Context,
+    symbolic_stack: Vec<Option<Box<BVBox>>>,
+    shadow_inputs: Option<BVBox>,
+    constraints: Vec<Constraint>,
     bits: u32,
 }
 
-impl<'a> ConcolicHost<'a> {
-    pub fn new(solver: Solver<'a>, ctx: Context, bytes: u32) -> Self {
+macro_rules! bv_from_u256 {
+    ($val:expr, $ctx:expr) => {{
+        let u64x4 = $val.0;
+        let bv = BV::from_u64(&ctx, u64x4[0], 64);
+        let bv = bv.concat(&BV::from_u64(&ctx, u64x4[1], 64));
+        let bv = bv.concat(&BV::from_u64(&ctx, u64x4[2], 64));
+        let bv = bv.concat(&BV::from_u64(&ctx, u64x4[3], 64));
+        bv
+    }};
+}
+
+impl ConcolicHost {
+    pub fn new(bytes: u32) -> Self {
         Self {
             env: Env::default(),
             data: HashMap::new(),
             code: HashMap::new(),
-            solver,
-            ctx,
             symbolic_stack: Vec::new(),
-            shadow_inputs: None,
+            shadow_inputs: Some(BVBox::new_input()),
+            constraints: vec![],
             bits: 8 * bytes,
         }
     }
 
-    pub fn set_shadow_inputs(&'a mut self) {
-        self.shadow_inputs = Some(BV::new_const(&self.ctx, "shadow_inputs", self.bits));
-    }
-
-    // pub fn get_bv_from_stack(&self, index: usize, interp: &mut Interpreter) -> BV {
-    //
-    // }
-
-    pub unsafe fn on_step(&'a mut self, interp: &mut Interpreter) {
-        // println!("{}", *interp.instruction_pointer);
-        macro_rules! bv_from_u256 {
-            ($val:expr) => {{
-                let u64x4 = $val.0;
-                let bv = BV::from_u64(&self.ctx, u64x4[0], 64);
-                let bv = bv.concat(&BV::from_u64(&self.ctx, u64x4[1], 64));
-                let bv = bv.concat(&BV::from_u64(&self.ctx, u64x4[2], 64));
-                let bv = bv.concat(&BV::from_u64(&self.ctx, u64x4[3], 64));
-                bv
-            }};
-        }
+    pub unsafe fn on_step(&mut self, interp: &mut Interpreter) {
         macro_rules! stack_bv {
             ($idx:expr) => {{
                 match self.symbolic_stack[$idx].borrow() {
                     Some(bv) => bv.clone(),
                     None => {
                         let u256 = interp.stack.peek($idx).expect("stack underflow");
-                        bv_from_u256!(u256)
+                        Box::new(BVBox {
+                            lhs: None,
+                            rhs: None,
+                            concrete: Some(u256),
+                            op: ConcolicOp::U256
+                        })
                     }
                 }
             }};
         }
 
-        let bv = match *interp.instruction_pointer {
+
+        let bv: Vec<Option<Box<BVBox>>> = match *interp.instruction_pointer {
             // ADD
             0x01 => {
                 vec![Some(stack_bv!(0).add(stack_bv!(1)))]
@@ -86,27 +257,27 @@ impl<'a> ConcolicHost<'a> {
             }
             // DIV - is this signed?
             0x04 => {
-                vec![Some(stack_bv!(0).bvsdiv(&stack_bv!(1)))]
+                vec![Some(stack_bv!(0).bvsdiv(stack_bv!(1)))]
             }
             // SDIV
             0x05 => {
-                vec![Some(stack_bv!(0).bvsdiv(&stack_bv!(1)))]
+                vec![Some(stack_bv!(0).bvsdiv(stack_bv!(1)))]
             }
             // MOD
             0x06 => {
-                vec![Some(stack_bv!(0).bvurem(&stack_bv!(1)))]
+                vec![Some(stack_bv!(0).bvurem(stack_bv!(1)))]
             }
             // SMOD
             0x07 => {
-                vec![Some(stack_bv!(0).bvsrem(&stack_bv!(1)))]
+                vec![Some(stack_bv!(0).bvsrem(stack_bv!(1)))]
             }
             // ADDMOD
             0x08 => {
-                vec![Some(stack_bv!(0).add(stack_bv!(1)).bvsrem(&stack_bv!(2)))]
+                vec![Some(stack_bv!(0).add(stack_bv!(1)).bvsrem(stack_bv!(2)))]
             }
             // MULMOD
             0x09 => {
-                vec![Some(stack_bv!(0).mul(stack_bv!(1)).bvsrem(&stack_bv!(2)))]
+                vec![Some(stack_bv!(0).mul(stack_bv!(1)).bvsrem(stack_bv!(2)))]
             }
             // EXP - we can't support, cuz z3 is bad at it
             0x0a => {
@@ -121,62 +292,54 @@ impl<'a> ConcolicHost<'a> {
             }
             // LT
             0x10 => {
-                self.solver.assert(
-                    &stack_bv!(0)
-                        .bvult(&stack_bv!(1))
-                        ._eq(&Bool::from_bool(&self.ctx, true)),
-                );
+                self.constraints.push(stack_bv!(0)
+                    .bvult(stack_bv!(1)));
                 vec![None]
             }
             // GT
             0x11 => {
-                self.solver.assert(
-                    &stack_bv!(0)
-                        .bvugt(&stack_bv!(1))
-                        ._eq(&Bool::from_bool(&self.ctx, true)),
-                );
+                self.constraints.push(stack_bv!(0)
+                    .bvugt(stack_bv!(1)));
                 vec![None]
             }
             // SLT
             0x12 => {
-                self.solver.assert(
-                    &stack_bv!(0)
-                        .bvslt(&stack_bv!(1))
-                        ._eq(&Bool::from_bool(&self.ctx, true)),
-                );
+                self.constraints.push(stack_bv!(0)
+                    .bvslt(stack_bv!(1)));
                 vec![None]
             }
             // SGT
             0x13 => {
-                self.solver.assert(
-                    &stack_bv!(0)
-                        .bvsgt(&stack_bv!(1))
-                        ._eq(&Bool::from_bool(&self.ctx, true)),
-                );
+                self.constraints.push(stack_bv!(0)
+                    .bvsgt(stack_bv!(1)));
                 vec![None]
             }
             // EQ
             0x14 => {
-                self.solver.assert(&stack_bv!(0)._eq(&stack_bv!(1)));
+                self.constraints.push(stack_bv!(0).eq(stack_bv!(1)));
                 vec![None]
             }
             // ISZERO
             0x15 => {
-                self.solver
-                    .assert(&stack_bv!(0)._eq(&BV::from_u64(&self.ctx, 0, 256)));
+                self.constraints.push(stack_bv!(0).eq(Box::new(BVBox {
+                        lhs: None,
+                        rhs: None,
+                        concrete: Some(U256::from(0)),
+                        op: ConcolicOp::U256
+                    })));
                 vec![None]
             }
             // AND
             0x16 => {
-                vec![Some(stack_bv!(0).bvand(&stack_bv!(1)))]
+                vec![Some(stack_bv!(0).bvand(stack_bv!(1)))]
             }
             // OR
             0x17 => {
-                vec![Some(stack_bv!(0).bvor(&stack_bv!(1)))]
+                vec![Some(stack_bv!(0).bvor(stack_bv!(1)))]
             }
             // XOR
             0x18 => {
-                vec![Some(stack_bv!(0).bvxor(&stack_bv!(1)))]
+                vec![Some(stack_bv!(0).bvxor(stack_bv!(1)))]
             }
             // NOT
             0x19 => {
@@ -189,15 +352,15 @@ impl<'a> ConcolicHost<'a> {
             }
             // SHL
             0x1b => {
-                vec![Some(stack_bv!(0).bvshl(&stack_bv!(1)))]
+                vec![Some(stack_bv!(0).bvshl(stack_bv!(1)))]
             }
             // SHR
             0x1c => {
-                vec![Some(stack_bv!(0).bvlshr(&stack_bv!(1)))]
+                vec![Some(stack_bv!(0).bvlshr(stack_bv!(1)))]
             }
             // SAR
             0x1d => {
-                vec![Some(stack_bv!(0).bvashr(&stack_bv!(1)))]
+                vec![Some(stack_bv!(0).bvsar(stack_bv!(1)))]
             }
             // SHA3
             0x20 => {
@@ -209,7 +372,7 @@ impl<'a> ConcolicHost<'a> {
             }
             // BALANCE
             0x31 => {
-                vec![Some(BV::new_const(&self.ctx, "balance", 256))]
+                vec![Some(Box::new(BVBox::new_balance()))]
             }
             // ORIGIN
             0x32 => {
@@ -221,14 +384,14 @@ impl<'a> ConcolicHost<'a> {
             }
             // CALLVALUE
             0x34 => {
-                vec![Some(BV::new_const(&self.ctx, "callvalue", 256))]
+                vec![Some(Box::new(BVBox::new_callvalue()))]
             }
             // CALLDATALOAD
             0x35 => {
                 // TODO(@shangying): can you please help me with this?
                 // basically, we need to get a new sub-BV from shadow input, which starts from
                 // `interp.stack.peek(0).unwrap().0[0] * 8` to that + 32 * 8 (32 bytes)
-                vec![None]
+                vec![Some(Box::new(BVBox::new_sliced_input(interp.stack.peek(0).unwrap())))]
             }
             // CALLDATASIZE
             0x36 => {
@@ -391,19 +554,20 @@ impl<'a> ConcolicHost<'a> {
             }
         };
         for v in bv {
-            self.symbolic_stack.push(v.clone());
+            self.symbolic_stack.push(v);
         }
     }
 }
 
-impl<'a> ConcolicHost<'a> {
+impl Host for ConcolicHost {
     const INSPECT: bool = true;
+    type DB = BenchmarkDB;
     // type DB = BenchmarkDB;
 
-    fn step(&'a mut self, interp: &mut Interpreter, is_static: bool) -> Return {
+    fn step(&mut self, interp: &mut Interpreter, is_static: bool) -> Return {
         unsafe {
             // self.symbolic_stack.push(Some(BV::from_u64(&self.ctx, 1, 2)))
-            self.on_step(interp);
+            // self.on_step(interp);
         }
 
         return Continue;
