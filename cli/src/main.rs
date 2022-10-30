a2e565faf14c74c9d5715d83e92ba074712957a5
shou <shou@berkeley.edu>
2022-10-29 19:59:55 -0700

    add clap!

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,11 +1,23 @@
 use std::path::PathBuf;
 use ityfuzz::fuzzers::basic_fuzzer;
+use clap::Parser;
+
+/// CLI for ItyFuzz
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Glob pattern to find contracts
+    #[arg(short, long)]
+    contract_glob: String,
+}
+
 
 fn main() {
+    let args = Args::parse();
     basic_fuzzer::dummyfuzzer(
         PathBuf::from("./tmp/corpus"),
         PathBuf::from("./tmp/objective"),
         PathBuf::from("./tmp/log"),
-        &String::from("../demo/*"),
+        &String::from(args.contract_glob),
     );
 }
