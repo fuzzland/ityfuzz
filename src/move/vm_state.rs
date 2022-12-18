173969d5e3f7b8e3e90518830c05728c0bf273e4
shou <shou@berkeley.edu>
2022-12-18 15:18:31 -0800

    refactor movevm executor

--- src/move/vm_state.rs
+++ src/move/vm_state.rs
@@ -4,35 +4,121 @@ use move_core_types::account_address::AccountAddress;
 use move_core_types::identifier::{IdentStr, Identifier};
 use move_core_types::language_storage::{ModuleId, StructTag};
 use move_core_types::resolver::{ModuleResolver, ResourceResolver};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::any::Any;
 use std::collections::HashMap;
+use move_binary_format::errors::{PartialVMResult, VMResult};
+use move_core_types::effects::Op;
+use move_core_types::gas_algebra::NumBytes;
+use move_core_types::value::MoveTypeLayout;
+use move_vm_runtime::interpreter::Frame;
+use move_vm_runtime::loader::Loader;
+use move_vm_runtime::move_vm::MoveVM;
+use move_vm_types::data_store::DataStore;
+use move_vm_types::loaded_data::runtime_types::Type;
+use move_vm_types::values::{GlobalValue, Value};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug)]
 pub struct MoveVMState {
-    pub modules: HashMap<MoveAddress, HashMap<Identifier, Vec<u8>>>,
-    pub resources: HashMap<MoveAddress, HashMap<StructTag, Vec<u8>>>,
+    pub resources: HashMap<AccountAddress, HashMap<Type, Value>>,
+    pub _gv_slot: HashMap<(AccountAddress, Type), GlobalValue>,
 }
 
-impl ModuleResolver for MoveVMState {
-    type Error = ();
-
-    fn get_module(&self, id: &ModuleId) -> Result<Option<Vec<u8>>, Self::Error> {
-        let module_account = self.modules.get(id.address()).unwrap();
-        return Ok(module_account.get(id.name()).cloned());
+impl Clone for MoveVMState {
+    fn clone(&self) -> Self {
+        assert!(self._gv_slot.is_empty());
+        MoveVMState {
+            resources: self.resources.clone(),
+            _gv_slot: HashMap::new(),
+        }
     }
 }
 
-impl ResourceResolver for MoveVMState {
-    type Error = ();
 
-    fn get_resource(
-        &self,
-        address: &AccountAddress,
-        typ: &StructTag,
-    ) -> Result<Option<Vec<u8>>, Self::Error> {
-        let resource_account = self.resources.get(address).unwrap();
-        return Ok(resource_account.get(typ).cloned());
+impl Serialize for MoveVMState {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+    {
+        unreachable!()
+    }
+}
+
+impl<'de> Deserialize<'de> for MoveVMState {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+    {
+        unreachable!()
+    }
+}
+
+impl MoveVMState {
+    pub fn commit(&mut self) {
+        let mut idx: usize = 0;
+        for ((addr, ty), gv) in self._gv_slot.iter() {
+            match gv.clone().into_effect() {
+                None => {}
+                Some(op) => {
+                    match op {
+                        Op::New(val) => {
+                            self.resources
+                                .entry(addr.clone())
+                                .or_insert(HashMap::new())
+                                .insert(ty.clone(), val.clone());
+                        }
+                        Op::Modify(val) => {
+                            self.resources
+                                .entry(addr.clone())
+                                .or_insert(HashMap::new())
+                                .insert(ty.clone(), val.clone());
+                        }
+                        Op::Delete => {
+                            self.resources
+                                .entry(addr.clone())
+                                .or_insert(HashMap::new())
+                                .remove(&ty);
+                        }
+                    }
+                }
+            }
+            idx += 1;
+        }
+        self._gv_slot.clear();
+    }
+}
+
+impl DataStore for MoveVMState {
+    fn load_resource(&mut self, addr: AccountAddress, ty: &Type) -> PartialVMResult<(&mut GlobalValue, Option<Option<NumBytes>>)> {
+        let data = self.resources.get(&addr)
+            .unwrap()
+            .get(ty)
+            .unwrap();
+
+        self._gv_slot.insert((addr, ty.clone()), GlobalValue::cached(data.clone()).unwrap());
+
+
+        return Ok((self._gv_slot.get_mut(&(addr, ty.clone())).unwrap(), None));
+    }
+
+    fn load_module(&self, module_id: &ModuleId) -> VMResult<Vec<u8>> {
+        unreachable!()
+    }
+
+    fn publish_module(&mut self, module_id: &ModuleId, blob: Vec<u8>, is_republishing: bool) -> VMResult<()> {
+        unreachable!()
+    }
+
+    fn exists_module(&self, module_id: &ModuleId) -> VMResult<bool> {
+        unreachable!()
+    }
+
+    fn emit_event(&mut self, guid: Vec<u8>, seq_num: u64, ty: Type, val: Value) -> PartialVMResult<()> {
+        unreachable!()
+    }
+
+    fn events(&self) -> &Vec<(Vec<u8>, u64, Type, MoveTypeLayout, Value)> {
+        unreachable!()
     }
 }
 
@@ -61,8 +147,8 @@ impl VMStateT for MoveVMState {
 impl Default for MoveVMState {
     fn default() -> Self {
         Self {
-            modules: HashMap::new(),
             resources: HashMap::new(),
+            _gv_slot: HashMap::new(),
         }
     }
 }
