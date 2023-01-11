311032a6b39f64bdbc68d2fb200d34f4670a686c
Shangyint <shangyin@berkeley.edu>
2023-01-11 06:39:08 -0800

    Serialize and Deserialize of ABI

--- src/abi.rs
+++ src/abi.rs
@@ -1,8 +1,8 @@
 use bytes::{Buf, BufMut, Bytes, BytesMut};
-use serde::{Deserialize, Deserializer, Serialize};
+use serde::{Deserialize, Serialize};
 
 // how can we deserialize this trait?
-pub trait ABI: CloneABI {
+pub trait ABI: CloneABI + serde_traitobject::Serialize + serde_traitobject::Deserialize{
     fn to_bytes(&self) -> &Bytes;
 }
 
@@ -21,59 +21,61 @@ where
 
 // Use BoxedABI so that downstream code knows the size of the trait object
 // However, Box makes deserilization difficult
-pub type BoxedABI = Box<dyn ABI>;
+#[derive(Serialize, Deserialize, Clone)]
+pub struct BoxedABI {
+    #[serde(with = "serde_traitobject")]
+    b: Box<dyn ABI>
+}
 
-// TODO: this does not work, need to figure out how to serialize/deserialize trait objects
-impl Serialize for BoxedABI {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let mut bytes = BytesMut::with_capacity(self.to_bytes().len());
-        bytes.put(&**self.to_bytes());
-        serializer.serialize_bytes(&bytes)
+impl BoxedABI {
+    pub fn new(b: Box<dyn ABI>) -> Self {
+        Self { b }
+    }
+
+    pub fn get(&self) -> &Box<dyn ABI> {
+        &self.b
     }
 }
 
-// TODO: implement Deserialize for BoxedABI
-
-impl Clone for BoxedABI {
-    fn clone(&self) -> BoxedABI {
+impl Clone for Box<dyn ABI> {
+    fn clone(&self) ->  Box<dyn ABI> {
         self.clone_box()
     }
 }
 
 // 0~256-bit data types
-#[derive(Clone)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct A256 {
     data: [u8; 32],
     data_len: usize,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct AFixedArray {
     data: Vec<BoxedABI>,
     data_len: usize,
     dynamic: bool,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct AArray {
     data: Vec<BoxedABI>,
     data_len: usize,
 }
 
+// FIXME: fix scope
 impl ABI for A256 {
     fn to_bytes(&self) -> &Bytes {
         return &Bytes::from(&self.data[..self.data_len]);
     }
 }
 
+// FIXME: fix scope
 impl ABI for AFixedArray {
     fn to_bytes(&self) -> &Bytes {
         let mut ret = BytesMut::new();
         for i in 0..self.data_len {
-            ret.extend_from_slice((*self.data[i]).to_bytes());
+            ret.extend_from_slice(self.data[i].b.to_bytes());
         }
         return &ret.copy_to_bytes(ret.len());
     }
@@ -83,4 +85,15 @@ impl ABI for AArray {
     fn to_bytes(&self) -> &Bytes {
         todo!()
     }
+}
+
+// test serialization and deserialization
+#[test]
+fn test_abi() {
+    let a = A256 {
+        data: [0; 32],
+        data_len: 32,
+    };
+    let c = serde_json::to_string(&a).unwrap();
+    println!("{}", c);
 }
