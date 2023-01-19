979b4ada0e5339f693a957daa22089842a61b1e0
shou <shou@berkeley.edu>
2023-01-19 13:52:44 -0800

    cargo fmt benches

--- benches/ityfuzz.rs
+++ benches/ityfuzz.rs
@@ -1,13 +1,11 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 // use crate::abi::get_abi_type_boxed;
 
-
 fn abi() -> u64 {
     todo!()
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-}
+fn criterion_benchmark(c: &mut Criterion) {}
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
