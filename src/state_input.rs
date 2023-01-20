6d0f244cb62504485c595f907478c9b421833b53
shou <shou@berkeley.edu>
2023-01-20 10:56:08 -0800

    add state crossover

--- src/state_input.rs
+++ src/state_input.rs
@@ -5,7 +5,7 @@ use serde::{Deserialize, Serialize};
 use std::path::Path;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct ItyVMState(VMState);
+pub struct ItyVMState(pub VMState);
 
 impl ItyVMState {
     pub fn new() -> Self {
@@ -14,25 +14,7 @@ impl ItyVMState {
 }
 
 impl Input for ItyVMState {
-    fn to_file<P>(&self, path: P) -> Result<(), Error>
-    where
-        P: AsRef<Path>,
-    {
-        todo!()
-    }
-
-    fn from_file<P>(path: P) -> Result<Self, Error>
-    where
-        P: AsRef<Path>,
-    {
-        todo!()
-    }
-
     fn generate_name(&self, idx: usize) -> String {
-        todo!()
-    }
-
-    fn wrapped_as_testcase(&mut self) {
-        // todo!()
+        format!("input-{}.state", idx)
     }
 }
