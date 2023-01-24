6113079a4534df499c59e129c512435677654220
shou <shou@berkeley.edu>
2023-01-24 12:15:05 -0800

    add infant feedback

---
+++ src/infant_feedback.rs
@@ -0,0 +1,36 @@
+use libafl::corpus::Testcase;
+use libafl::Error;
+use libafl::events::EventFirer;
+use libafl::executors::ExitKind;
+use libafl::observers::ObserversTuple;
+use libafl::prelude::{Feedback, Named};
+use libafl::state::State;
+
+#[derive(Debug)]
+pub struct InfantFeedback {
+}
+
+impl Named for InfantFeedback {
+    fn name(&self) -> &str {
+        "InfantFeedback"
+    }
+}
+
+impl<I, S> Feedback<I, S> for InfantFeedback
+where S: State {
+    fn init_state(&mut self, _state: &mut S) -> Result<(), Error> {
+        todo!()
+    }
+
+    fn is_interesting<EM, OT>(&mut self, state: &mut S, manager: &mut EM, input: &I, observers: &OT, exit_kind: &ExitKind) -> Result<bool, Error> where EM: EventFirer<I>, OT: ObserversTuple<I, S> {
+        todo!()
+    }
+
+    fn append_metadata(&mut self, _state: &mut S, _testcase: &mut Testcase<I>) -> Result<(), Error> {
+        todo!()
+    }
+
+    fn discard_metadata(&mut self, _state: &mut S, _input: &I) -> Result<(), Error> {
+        todo!()
+    }
+}
