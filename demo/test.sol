b103face73d494e2fc8d943344c4505d4879e7a5
shou <shou@berkeley.edu>
2023-01-31 02:42:52 -0800

    fix loading interfaces

--- demo/test.sol
+++ demo/test.sol
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.15;
 
+interface X {
+    function fx() external returns (uint);
+}
+
 contract main {
     mapping(uint256 => uint256) knownsec;
 
     // solution: a = 1
     function process(uint256 a) public returns (string memory){
+        X x = X(0x0010000000000000000000000000000000000001);
+        x.fx();
         require(a < 2, "2");
         knownsec[2] = a;
         return 'Hello Contracts';
---
+++ demo/x.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+
+contract x {
+    function fx() external returns (uint){
+        return 1;
+    }
+}
