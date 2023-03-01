9cde8f30a652689e3975e945758a7778fc31b301
Chaofan Shou <shou@berkeley.edu>
2023-03-01 09:37:32 -0800

    add integration tests and fix some broken examples

--- tests/verilog-2/Bounty.sol
+++ tests/verilog-2/Bounty.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.10;
+pragma solidity 0.8.15;
 
 import {IERC20} from "./interfaces/IERC20.sol";
 import {IWMATIC} from "./interfaces/IWMATIC.sol";
--- tests/verilog-2/interfaces/IUniswapV2Pair.sol
+++ tests/verilog-2/interfaces/IUniswapV2Pair.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.10;
+pragma solidity 0.8.15;
 
 interface IUniswapV2Pair {
     event Approval(address indexed owner, address indexed spender, uint256 value);
