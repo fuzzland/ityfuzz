7bfc52219a508c6146e0bbf79fadf2706a3bbe86
shou <shou@berkeley.edu>
2023-03-27 11:53:53 -0700

    refactor middleware

--- src/evm/onchain/onchain.rs
+++ src/evm/onchain/onchain.rs
@@ -3,7 +3,7 @@ use crate::evm::config::StorageFetchingMode;
 use crate::evm::contract_utils::ContractLoader;
 use crate::evm::input::EVMInput;
 use crate::evm::middleware::MiddlewareOp::{AddCorpus, UpdateCode, UpdateSlot};
-use crate::evm::middleware::{CanHandleDeferredActions, Middleware, MiddlewareOp, MiddlewareType};
+use crate::evm::middleware::{add_corpus, Middleware, MiddlewareOp, MiddlewareType};
 use crate::evm::onchain::endpoints::OnChainConfig;
 use crate::evm::vm::{FuzzHost, IntermediateExecutionResult};
 use crate::generic_vm::vm_state::VMStateT;
@@ -25,6 +25,7 @@ use std::collections::{HashMap, HashSet};
 use std::fmt::{Debug, Formatter};
 use std::sync::Arc;
 use std::time::Duration;
+use nix::libc::stat;
 
 const UNBOUND_THRESHOLD: usize = 30;
 
@@ -39,12 +40,11 @@ where
     pub calls: HashMap<(H160, usize), HashSet<H160>>,
     pub locs: HashMap<(H160, usize), HashSet<U256>>,
     pub endpoint: OnChainConfig,
-    pub scheduler: Option<Box<dyn Scheduler<I, S>>>,
     pub blacklist: HashSet<H160>,
     pub storage_fetching: StorageFetchingMode,
     pub storage_all: HashMap<H160, Arc<HashMap<String, U256>>>,
     pub storage_dump: HashMap<H160, Arc<HashMap<U256, U256>>>,
-    pub phantom: std::marker::PhantomData<VS>,
+    pub phantom: std::marker::PhantomData<(I, S, VS)>,
 }
 
 impl<VS, I, S> Debug for OnChain<VS, I, S>
@@ -68,21 +68,16 @@ where
     S: State,
     VS: VMStateT + Default,
 {
-    pub fn new<SC>(
+    pub fn new(
         endpoint: OnChainConfig,
-        scheduler: SC,
         storage_fetching: StorageFetchingMode,
-    ) -> Self
-    where
-        SC: Scheduler<I, S> + 'static,
-    {
+    ) -> Self {
         Self {
             loaded_data: Default::default(),
             loaded_code: Default::default(),
             calls: Default::default(),
             locs: Default::default(),
             endpoint,
-            scheduler: Some(Box::new(scheduler)),
             blacklist: Default::default(),
             storage_all: Default::default(),
             storage_dump: Default::default(),
@@ -106,13 +101,13 @@ pub fn keccak_hex(data: U256) -> String {
     hex::encode(&output).to_string()
 }
 
-impl<VS, I, S> Middleware for OnChain<VS, I, S>
+impl<VS, I, S> Middleware<S> for OnChain<VS, I, S>
 where
     I: Input + VMInputT<VS, H160, H160> + 'static,
-    S: State + std::fmt::Debug + 'static,
+    S: State + Debug + HasCaller<H160> + HasCorpus<I> + HasItyState<H160, H160, VS>  + HasMetadata + Clone + 'static,
     VS: VMStateT + Default + 'static,
 {
-    unsafe fn on_step(&mut self, interp: &mut Interpreter) -> Vec<MiddlewareOp> {
+    unsafe fn on_step(&mut self, interp: &mut Interpreter, host: &mut FuzzHost<S>, state: &mut S) {
         let pc = interp.program_counter();
         #[cfg(feature = "force_cache")]
         macro_rules! force_cache {
@@ -142,8 +137,8 @@ where
 
         match *interp.instruction_pointer {
             0x54 => {
-                let slot_idx = interp.stack.peek(0).unwrap();
                 let address = interp.contract.address;
+                let slot_idx = interp.stack.peek(0).unwrap();
 
                 macro_rules! load_data {
                     ($func: ident, $stor: ident, $key: ident) => {{
@@ -163,69 +158,68 @@ where
                     }};
                     () => {};
                 }
+                macro_rules! slot_val {
+                    () => {
+                        {
+                            match self.storage_fetching {
+                                StorageFetchingMode::Dump => {
+                                    load_data!(fetch_storage_dump, storage_dump, slot_idx)
+                                }
+                                StorageFetchingMode::All => {
+                                    // the key is in keccak256 format
+                                    let key = keccak_hex(slot_idx);
+                                    load_data!(fetch_storage_all, storage_all, key)
+                                }
+                                StorageFetchingMode::OneByOne => self.endpoint.get_contract_slot(
+                                    address,
+                                    slot_idx,
+                                    force_cache!(self.locs, slot_idx),
+                                ),
+                            }
+                        }
+                    };
+                }
 
-                let slot_val = {
-                    match self.storage_fetching {
-                        StorageFetchingMode::Dump => {
-                            load_data!(fetch_storage_dump, storage_dump, slot_idx)
+                match host.data.get_mut(&address) {
+                    Some(data) => {
+                        if data.get(&slot_idx).is_none() {
+                            data.insert(slot_idx, slot_val!());
                         }
-                        StorageFetchingMode::All => {
-                            // the key is in keccak256 format
-                            let key = keccak_hex(slot_idx);
-                            load_data!(fetch_storage_all, storage_all, key)
-                        }
-                        StorageFetchingMode::OneByOne => self.endpoint.get_contract_slot(
-                            address,
-                            slot_idx,
-                            force_cache!(self.locs, slot_idx),
-                        ),
                     }
-                };
-                vec![UpdateSlot(
-                    MiddlewareType::OnChain,
-                    address,
-                    slot_idx,
-                    slot_val,
-                )]
+                    None => {
+                        let mut data = HashMap::new();
+                        data.insert(slot_idx, slot_val!());
+                        host.data.insert(address, data);
+                    }
+                }
             }
 
             0xf1 | 0xf2 | 0xf4 | 0xfa => {
                 let address = interp.stack.peek(1).unwrap();
                 let address_h160 = convert_u256_to_h160(address);
                 if self.blacklist.contains(&address_h160) {
-                    return vec![];
+                    return;
                 }
                 let force_cache = force_cache!(self.calls, address_h160);
 
                 let contract_code = self.endpoint.get_contract_code(address_h160, force_cache);
-                let has_code = !contract_code.is_empty();
 
-                let code_update = UpdateCode(MiddlewareType::OnChain, address_h160, contract_code);
-
-                let abi = if !self.loaded_code.contains(&address_h160) && !force_cache && has_code {
-                    self.endpoint.fetch_abi(address_h160)
-                } else {
-                    None
-                };
-
-                self.loaded_code.insert(address_h160);
-
-                match abi {
-                    Some(abi_ins) => {
-                        // AddCorpus(MiddlewareType::OnChain, )
-                        vec![
-                            code_update,
-                            AddCorpus(MiddlewareType::OnChain, abi_ins, address_h160),
-                        ]
-                    }
-                    None => {
-                        vec![code_update]
+                if !self.loaded_code.contains(&address_h160)
+                    && !force_cache
+                    && !contract_code.is_empty() {
+                    let abi = self.endpoint.fetch_abi(address_h160);
+                    self.loaded_code.insert(address_h160);
+                    match abi {
+                        Some(ref abi_ins) => {
+                            add_corpus(host, address_h160, abi_ins, state);
+                        }
+                        None => {}
                     }
                 }
+
+                host.set_code(address_h160, contract_code);
             }
-            _ => {
-                vec![]
-            }
+            _ => {}
         }
     }
 
@@ -233,67 +227,4 @@ where
         MiddlewareType::OnChain
     }
 
-    fn as_any(&mut self) -> &mut (dyn Any + 'static) {
-        self
-    }
-}
-
-impl<VS, I, S> CanHandleDeferredActions<VS, S> for OnChain<VS, I, S>
-where
-    I: Input + VMInputT<VS, H160, H160> + 'static,
-    S: State + HasCorpus<I> + HasItyState<H160, H160, VS> + HasMetadata + HasCaller<H160> + 'static,
-    VS: VMStateT + Default,
-{
-    fn handle_deferred_actions(
-        &mut self,
-        op: &MiddlewareOp,
-        state: &mut S,
-        result: &mut IntermediateExecutionResult,
-    ) {
-        match op {
-            MiddlewareOp::AddCorpus(.., input, address) => {
-                state.add_address(address);
-                ContractLoader::parse_abi_str(input)
-                    .iter()
-                    .filter(|v| !v.is_constructor)
-                    .for_each(|abi| {
-                        #[cfg(not(feature = "fuzz_static"))]
-                        if abi.is_static {
-                            return;
-                        }
-
-                        let mut abi_instance = get_abi_type_boxed(&abi.abi);
-                        abi_instance.set_func_with_name(abi.function, abi.function_name.clone());
-                        let input = EVMInput {
-                            caller: state.get_rand_caller(),
-                            contract: address.clone(),
-                            data: Some(abi_instance),
-                            sstate: StagedVMState::new_uninitialized(),
-                            sstate_idx: 0,
-                            txn_value: if abi.is_payable { Some(0) } else { None },
-                            step: false,
-
-                            #[cfg(test)]
-                            direct_data: Default::default(),
-                        };
-                        let mut tc =
-                            Testcase::new(input.as_any().downcast_ref::<I>().unwrap().clone())
-                                as Testcase<I>;
-                        tc.set_exec_time(Duration::from_secs(0));
-                        let idx = state.corpus_mut().add(tc).expect("failed to add");
-                        self.scheduler
-                            .as_ref()
-                            .unwrap()
-                            .on_add(state, idx)
-                            .expect("failed to call scheduler on_add");
-                    });
-            }
-            MiddlewareOp::AddBlacklist(.., address) => {
-                self.blacklist.insert(address.clone());
-            }
-            _ => {
-                panic!("MiddlewareOp::execute_with_state called with invalid op");
-            }
-        }
-    }
 }
