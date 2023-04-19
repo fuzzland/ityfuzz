04731377fd9e289269eea41b09d7a44aba9eb0a3
Chaofan Shou <shou@berkeley.edu>
2023-04-19 09:27:04 -0700

    fix unknown src

--- onchain_scripts/proxy/main.py
+++ onchain_scripts/proxy/main.py
@@ -299,7 +299,7 @@ def get_pair_pegged(token, network, block):
                     "decimals0": pair["token0"]["decimals"],
                     "decimals1": pair["token1"]["decimals"],
                     "src_exact": name,
-                    "src": "v2_pegged",
+                    "src": "v2",
                 })
     return next_tokens
 
