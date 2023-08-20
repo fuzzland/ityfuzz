6378a47aa8bcc5db0e559ed90653ab45eb5713a1
0xAWM <29773064+0xAWM@users.noreply.github.com>
2023-08-20 15:44:34 +0800

    fix bug

--- src/evm/middlewares/call_printer.rs
+++ src/evm/middlewares/call_printer.rs
@@ -1,48 +1,57 @@
+use crate::evm::host::FuzzHost;
+use crate::evm::input::{ConciseEVMInput, EVMInput, EVMInputT};
+use crate::evm::middlewares::middleware::{Middleware, MiddlewareType};
+use crate::evm::srcmap::parser::SourceMapAvailability::Available;
+use crate::evm::srcmap::parser::{
+    pretty_print_source_map, SourceMapAvailability, SourceMapLocation, SourceMapWithCode,
+};
+use crate::evm::types::{as_u64, convert_u256_to_h160, is_zero, EVMAddress, ProjectSourceMapTy};
+use crate::evm::vm::IN_DEPLOY;
+use crate::generic_vm::vm_state::VMStateT;
+use crate::input::VMInputT;
+use crate::state::{HasCaller, HasCurrentInputIdx, HasItyState};
+use itertools::Itertools;
+use libafl::inputs::Input;
+use libafl::prelude::{HasCorpus, HasMetadata, State};
+use revm_interpreter::opcode::{INVALID, JUMPDEST, JUMPI, REVERT, STOP};
+use revm_interpreter::Interpreter;
+use revm_primitives::Bytecode;
+use serde::{Deserialize, Serialize};
+use serde_json;
 use std::collections::{HashMap, HashSet};
-use std::fmt::{Debug};
+use std::fmt::Debug;
 use std::fs;
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::ops::AddAssign;
 use std::path::Path;
 use std::time::{SystemTime, UNIX_EPOCH};
-use itertools::Itertools;
-use libafl::inputs::Input;
-use libafl::prelude::{HasCorpus, HasMetadata, State};
-use revm_interpreter::Interpreter;
-use revm_interpreter::opcode::{INVALID, JUMPDEST, JUMPI, REVERT, STOP};
-use revm_primitives::Bytecode;
-use serde::{Deserialize, Serialize};
-use crate::evm::host::FuzzHost;
-use crate::evm::input::{ConciseEVMInput, EVMInput, EVMInputT};
-use crate::evm::middlewares::middleware::{Middleware, MiddlewareType};
-use crate::evm::srcmap::parser::{pretty_print_source_map, SourceMapAvailability, SourceMapLocation, SourceMapWithCode};
-use crate::evm::srcmap::parser::SourceMapAvailability::Available;
-use crate::generic_vm::vm_state::VMStateT;
-use crate::input::VMInputT;
-use crate::state::{HasCaller, HasCurrentInputIdx, HasItyState};
-use crate::evm::types::{as_u64, convert_u256_to_h160, EVMAddress, is_zero, ProjectSourceMapTy};
-use crate::evm::vm::IN_DEPLOY;
-use serde_json;
 
 #[derive(Clone, Debug, Serialize, Default, Deserialize)]
 pub struct CallPrinter {
     pub layer: usize,
-    pub data: String
+    pub data: String,
 }
-
 
 impl CallPrinter {
     pub fn new() -> Self {
-        Self { layer: 0, data: "".to_string() }
+        Self {
+            layer: 0,
+            data: "".to_string(),
+        }
     }
 
     pub fn register_input(&mut self, input: &EVMInput) {
         self.layer = 0;
         self.data = "".to_string();
         self.data.push_str(
-            format!("[{:?}]=>{:?} {}", input.get_caller(), input.get_contract(), hex::encode(input.to_bytes()))
-                .as_str()
+            format!(
+                "[{:?}]=>{:?} {}",
+                input.get_caller(),
+                input.get_contract(),
+                hex::encode(input.to_bytes())
+            )
+            .as_str(),
         )
     }
 
@@ -51,12 +60,11 @@ impl CallPrinter {
     }
 }
 
-
 impl<I, VS, S> Middleware<VS, I, S> for CallPrinter
-    where
-        I: Input + VMInputT<VS, EVMAddress, EVMAddress, ConciseEVMInput> + EVMInputT + 'static,
-        VS: VMStateT,
-        S: State
+where
+    I: Input + VMInputT<VS, EVMAddress, EVMAddress, ConciseEVMInput> + EVMInputT + 'static,
+    VS: VMStateT,
+    S: State
         + HasCaller<EVMAddress>
         + HasCorpus<I>
         + HasItyState<EVMAddress, EVMAddress, VS, ConciseEVMInput>
@@ -72,18 +80,8 @@ impl<I, VS, S> Middleware<VS, I, S> for CallPrinter
         state: &mut S,
     ) {
         let (arg_offset, arg_len) = match unsafe { *interp.instruction_pointer } {
-            0xf1 | 0xf2 => {
-                (
-                    interp.stack.peek(3).unwrap(),
-                    interp.stack.peek(4).unwrap(),
-                )
-            }
-            0xf4 | 0xfa => {
-                (
-                    interp.stack.peek(2).unwrap(),
-                    interp.stack.peek(3).unwrap(),
-                )
-            }
+            0xf1 | 0xf2 => (interp.stack.peek(3).unwrap(), interp.stack.peek(4).unwrap()),
+            0xf4 | 0xfa => (interp.stack.peek(2).unwrap(), interp.stack.peek(3).unwrap()),
             _ => {
                 return;
             }
@@ -94,7 +92,11 @@ impl<I, VS, S> Middleware<VS, I, S> for CallPrinter
         let arg_offset = as_u64(arg_offset) as usize;
         let arg_len = as_u64(arg_len) as usize;
 
-        let arg = interp.memory.get_slice(arg_offset, arg_len);
+        let arg = if arg_len == 0 {
+            vec![]
+        } else {
+            interp.memory.get_slice(arg_offset, arg_len).to_vec()
+        };
 
         let caller = interp.contract.address;
         let address = match *interp.instruction_pointer {
@@ -114,9 +116,8 @@ impl<I, VS, S> Middleware<VS, I, S> for CallPrinter
                 address_h160,
                 hex::encode(arg)
             )
-                .as_str(),
+            .as_str(),
         );
-
     }
 
     unsafe fn on_return(
@@ -128,12 +129,16 @@ impl<I, VS, S> Middleware<VS, I, S> for CallPrinter
         self.layer -= 1;
     }
 
-    unsafe fn on_insert(&mut self, bytecode: &mut Bytecode, address: EVMAddress, host: &mut FuzzHost<VS, I, S>, state: &mut S) {
-
+    unsafe fn on_insert(
+        &mut self,
+        bytecode: &mut Bytecode,
+        address: EVMAddress,
+        host: &mut FuzzHost<VS, I, S>,
+        state: &mut S,
+    ) {
     }
 
     fn get_type(&self) -> MiddlewareType {
         MiddlewareType::CallPrinter
     }
 }
-
