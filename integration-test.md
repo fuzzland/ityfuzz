1b8e74eb5de313ef957ef25aae70cfb25303dc1f
zekai <zekai@zekais-MacBook-Pro.local>
2023-08-31 07:01:30 +0800

    Add onchain test.

--- integration-test.md
+++ integration-test.md
@@ -8,12 +8,10 @@ This will attempt to see whether the fuzzer can hit `bug()` in all projects in t
 
 
 ## On-chain Changes
-Ensure you build with `flashloan_v2` and have a local proxy running at `http://localhost:5003`.
-
 Following command shall find bugs:
 ```bash
-./target/release/cli -o -t 0xBcF6e9d27bf95F3F5eDDB93C38656D684317D5b4,0x5d6c48f05ad0fde3f64bab50628637d73b1eb0bb -c POLYGON --onchain-block-number 35690977  -f -i -p --onchain-local-proxy-addr http://localhost:5003
-./target/release/cli -o -t 0x10ED43C718714eb63d5aA57B78B54704E256024E,0xdDc0CFF76bcC0ee14c3e73aF630C029fe020F907,0x40eD17221b3B2D8455F4F1a05CAc6b77c5f707e3 -c BSC --onchain-block-number 23695904 -f -i --onchain-local-proxy-addr http://localhost:5003
+./target/release/cli evm -o -t 0xBcF6e9d27bf95F3F5eDDB93C38656D684317D5b4,0x5d6c48f05ad0fde3f64bab50628637d73b1eb0bb -c POLYGON --onchain-block-number 35690977  -f -i -p
+./target/release/cli evm -o -t 0x10ED43C718714eb63d5aA57B78B54704E256024E,0xdDc0CFF76bcC0ee14c3e73aF630C029fe020F907,0x40eD17221b3B2D8455F4F1a05CAc6b77c5f707e3 -c BSC --onchain-block-number 23695904 -f -i
 ```
 
 Following command shall not find bugs:
--- integration_test.py
+++ integration_test.py
@@ -52,8 +52,54 @@ def test_one(path):
 
     print(f"=== Success: {path}, Finished in {time.time() - start_time}s")
 
+def test_onchain(contract_addresses, block_number):
+    cmd = [
+        TIMEOUT_BIN, 
+        "3m",
+        "./cli/target/release/cli", "evm", "-o", 
+        "-t", ",".join(contract_addresses), 
+        "-c", "POLYGON", 
+        "--onchain-block-number", str(block_number), 
+        "-f", "-i", "-p"
+    ]
+
+    start_time = time.time()
+
+    p = subprocess.run(" ".join(cmd),
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE,
+                       shell=True)
+
+    if b"Found violations!" not in p.stdout:
+        print("================ STDERR =================")
+        print(p.stderr.decode("utf-8"))
+        print("================ STDOUT =================")
+        print(p.stdout.decode("utf-8"))
+        print(f"Failed to test onchain for contracts: {', '.join(contract_addresses)}")
+
+    print(f"=== Success: Tested onchain for contracts: {', '.join(contract_addresses)}, Finished in {time.time() - start_time}s")
+
 
 def build_fuzzer():
+    # build fuzzer
+    os.chdir("cli")
+    subprocess.run(["cargo", "build", "--release"])
+    os.chdir("..")
+
+def update_cargo_toml():
+    with open("Cargo.toml", "r") as file:
+        content = file.read()
+
+    if '"cmp"' in content:
+        content = content.replace('"cmp"', '"cmp","flashloan_v2"')
+
+    with open("Cargo.toml", "w") as file:
+        file.write(content)
+
+    print("Cargo.toml has been updated!")
+
+def build_flash_loan_v2_fuzzer():
+    update_cargo_toml()
     # build fuzzer
     os.chdir("cli")
     subprocess.run(["cargo", "build", "--release"])
@@ -66,3 +112,8 @@ if __name__ == "__main__":
     build_fuzzer()
     with multiprocessing.Pool(3) as p:
         p.map(test_one, glob.glob("./tests/evm/*/", recursive=True))
+
+    build_flash_loan_v2_fuzzer()
+    contract_addresses = ["0xBcF6e9d27bf95F3F5eDDB93C38656D684317D5b4", "0x5d6c48f05ad0fde3f64bab50628637d73b1eb0bb"]
+    block_number = 35690977
+    test_onchain(contract_addresses, block_number)
