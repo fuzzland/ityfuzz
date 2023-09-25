c61315e11d3ea743f7de21af2933f16c706f5389
Cloud10240 <136277643+Cloud10240@users.noreply.github.com>
2023-09-26 00:39:08 +0800

    Add onchain tests in ci. (#194)
    * Add onchain tests in ci.
    
    * Check input in test_onchain.
    
    * Fix bug.
    
    * CI test using cache.
    
    * fix onchain ci
    
    * ready
    
    * add ci
    
    * reduce onchain tests
    
    * further reduce tests
    
    * read rpc & keys from env
    
    * add polygon
    
    * add logs
    
    * fix ci crash
    
    ---------
    
    Co-authored-by: shouc <shou@berkeley.edu>
    Co-authored-by: shouc <scf@ieee.org>

---
+++ ci.py
@@ -0,0 +1,97 @@
+import os
+import requests
+import uuid
+
+def post_comment_to_pr(pr_number, token, message):
+    """
+    Post a comment to a GitHub PR.
+    
+    Parameters:
+        pr_number (int): The PR number.
+        token (str): The GitHub token to authenticate with.
+        message (str): The comment message.
+    """
+    url = f"https://api.github.com/repos/{owner}/{repo}/issues/{pr_number}/comments"
+    headers = {
+        "Authorization": f"token {token}",
+        "User-Agent": "PR-Commenter",
+        "Accept": "application/vnd.github.v3+json",
+    }
+    data = {"body": message}
+    
+    response = requests.post(url, headers=headers, json=data)
+    
+    if response.status_code == 201:
+        print("Successfully posted the comment!")
+    else:
+        print(f"Failed to post comment. Status code: {response.status_code}, Response: {response.text}")
+
+
+
+DEFAULT_MD = """| Project Name | Vulnerability Found | Time Taken | Log |
+|---------|---------|---------|---------|"""
+
+def parse_res(file):
+    with open(UID + "/" + file, 'r') as f:
+        lines = f.readlines()
+        last_ts = -1
+        ts = -1
+        violation = ""
+        crashed = False
+        for i in lines:
+            if "run time: " in i:
+                _ts = i.split("run time: ")[1].split(",")[0]
+                last_ts = _ts
+            if "more than owed" in i:
+                ts = last_ts
+                violation = "Fund Loss"
+            if "Reserves changed from" in i:
+                ts = last_ts
+                violation = "Price Manipulation"
+
+            if "Arbitrary call " in i:
+                ts = last_ts
+                violation = "Arbitrary Call"
+            if "panicked at " in i:
+                crashed = True
+        violation = "✅ " + violation  if violation else "❌"
+
+        if crashed:
+            violation = "❌‼️  Crashed"
+
+        return (file.replace("res_", ""), ts, violation)
+
+
+UID = str(uuid.uuid4())
+
+def parse_all():
+    os.system(f"mkdir {UID} && mv res_* {UID} && aws s3 cp {UID} s3://cilogs-ityfuzz/{UID} --recursive")
+
+    found = 0
+    md = DEFAULT_MD
+    for i in os.listdir(UID):
+        if i.startswith("res_"):
+            fn, ts, violation = parse_res(i)
+            if "❌" not in violation:
+                found += 1
+            log = f"https://cilogs-ityfuzz.s3.amazonaws.com/{UID}/{i}"
+            md += f"\n| {fn} | {violation} | {ts} | [Log File]({log}) |"
+    return f"Found: {found}\n\n" + md
+
+if __name__ == "__main__":
+    PR_NUMBER = os.environ.get("PR_NUMBER")
+    GITHUB_TOKEN = os.environ.get("BOT_GH_TOKEN")
+    owner = "fuzzland"
+    repo = "ityfuzz"
+
+    comment_message = parse_all()
+    print(comment_message)
+    if not PR_NUMBER or not GITHUB_TOKEN:
+        print("Missing PR_NUMBER or GITHUB_TOKEN environment variables!")
+        exit(1)
+
+
+    post_comment_to_pr(PR_NUMBER, GITHUB_TOKEN, comment_message)
+
+
+
--- integration_test.py
+++ integration_test.py
@@ -1,4 +1,5 @@
 import glob
+import random
 import subprocess
 import os
 import time
@@ -6,6 +7,14 @@ import time
 
 TIMEOUT_BIN = "timeout" if os.name == "posix" else "gtimeout"
 
+def read_onchain_tests():
+    tests = ""
+    with open("onchain_tests.txt", "r") as file:
+        tests = file.read()
+    
+    tests = tests.split("\n")
+    tests = [test.split("\t") for test in tests]
+    return tests
 
 def test_one(path):
     # cleanup
@@ -44,14 +53,70 @@ def test_one(path):
         print(p.stderr.decode("utf-8"))
         print("================ STDOUT =================")
         print(p.stdout.decode("utf-8"))
-        print(f"Failed to fuzz {path}")
+        print(f"=== Failed to fuzz {path}")
+    else:
+        print(f"=== Success: {path}, Finished in {time.time() - start_time}s")
 
     # clean up
     # os.system(f"rm -rf {path}/*.abi")
     # os.system(f"rm -rf {path}/*.bin")
 
-    print(f"=== Success: {path}, Finished in {time.time() - start_time}s")
 
+def test_onchain(test):
+
+    if len(test) != 4:
+        print(f"=== Invalid test: {test}")
+        return
+
+    # randomly sleep for 0 - 30s to avoid peak traffic
+    time.sleep(60 * random.random())
+
+    contract_addresses, block_number, chain, name= test[3], test[2], test[1], test[0]
+    
+    if chain not in ["eth", "bsc", "polygon"]:
+        print(f"=== Unsupported chain: {chain}")
+        return
+    
+    etherscan_key = os.getenv(f"{chain.upper()}_ETHERSCAN_API_KEY")
+    if etherscan_key is None:
+        print(f"=== No etherscan api key for {chain}")
+        return
+    my_env = os.environ.copy()
+    my_env["ETH_RPC_URL"] = os.getenv(f"{chain.upper()}_RPC_URL")
+
+    cmd = [
+        TIMEOUT_BIN, 
+        # set timeout to 5m because it takes longer time to sync the chain
+        "5m",
+        "./cli/target/release/cli", "evm", "-o", 
+        "-t", contract_addresses, 
+        "-c", chain, 
+        "--onchain-block-number", str(block_number), 
+        "-f", "-i", "-p", 
+        "--onchain-etherscan-api-key", etherscan_key,  
+        "--work-dir", f"w_{name}", 
+        #"--run-forever"
+    ]
+
+    start_time = time.time()
+
+    # try 3 times in case of rpc failure
+    for i in range(3):
+        
+        p = subprocess.run(" ".join(cmd),
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE,
+                        shell=True, env=my_env)
+
+        if b"Found violations!" in p.stdout:
+            print(f"=== Success: Tested onchain for contracts: {name}, Finished in {time.time() - start_time}s")
+            open(f"res_{name}.txt", "w+").write(p.stderr.decode("utf-8") + " ".join(cmd) + "\n" + p.stdout.decode("utf-8"))
+            return
+        time.sleep(30)
+
+
+    print(f"=== Failed to test onchain for contracts: {name}")
+    open(f"res_{name}.txt", "w+").write(p.stderr.decode("utf-8") + " ".join(cmd) + "\n" + p.stdout.decode("utf-8"))
 
 def build_fuzzer():
     # build fuzzer
@@ -59,10 +124,51 @@ def build_fuzzer():
     subprocess.run(["cargo", "build", "--release"])
     os.chdir("..")
 
+def update_cargo_toml():
+    with open("Cargo.toml", "r") as file:
+        content = file.read()
+
+    if '"flashloan_v2"' in content:
+        return
+
+    if '"cmp"' in content:
+        content = content.replace('"cmp"', '"cmp","flashloan_v2","force_cache"')
+
+    with open("Cargo.toml", "w") as file:
+        file.write(content)
+
+    print("Cargo.toml has been updated!")
+
+def build_flash_loan_v2_fuzzer():
+    update_cargo_toml()
+    # build fuzzer
+    os.chdir("cli")
+    subprocess.run(["cargo", "build", "--release"])
+    os.chdir("..")
+
 
 import multiprocessing
+import sys
+
 
 if __name__ == "__main__":
-    build_fuzzer()
-    with multiprocessing.Pool(3) as p:
-        p.map(test_one, glob.glob("./tests/evm/*/", recursive=True))
+    actions = []
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "onchain":
+            actions.append("onchain")
+        elif sys.argv[1] == "offchain":
+            actions.append("offchain")
+    else:
+        actions = ["onchain", "offchain"]
+
+    if "offchain" in actions:
+        build_fuzzer()
+        with multiprocessing.Pool(3) as p:
+            p.map(test_one, glob.glob("./tests/evm/*/", recursive=True))
+
+    if "onchain" in actions:
+        build_flash_loan_v2_fuzzer()
+        tests = read_onchain_tests()
+        with multiprocessing.Pool(10) as p:
+            p.map(test_onchain, tests)
