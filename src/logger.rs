d6e2d8bbc6946a816394929f831d20036bda6918
Jacob Chia <jacob-chia@outlook.com>
2024-04-09 09:38:26 +0800

    test_logger ignores error

--- src/logger.rs
+++ src/logger.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
@@ -7,18 +8,19 @@ pub fn init() {
     } else {
         Level::INFO
     };
-    init_logger(level, false);
+    init_logger(level, false).expect("Failed to initialize logger");
 }
 
 pub fn init_test() {
-    init_logger(Level::DEBUG, true);
+    let _ = init_logger(Level::DEBUG, true);
 }
 
-fn init_logger(level: Level, with_target: bool) {
+fn init_logger(level: Level, with_target: bool) -> Result<()> {
     let subscriber_builder = FmtSubscriber::builder()
         .compact()
         .with_target(with_target)
         .without_time();
     let subscriber = subscriber_builder.with_max_level(level).finish();
-    tracing::subscriber::set_global_default(subscriber).expect("failed to initialize logger");
+    tracing::subscriber::set_global_default(subscriber)?;
+    Ok(())
 }
