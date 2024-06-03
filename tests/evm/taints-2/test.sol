0b11ad41bfa9dcd424ce9b4a3d6453281b7258e5
Jacob Chia <jacob-chia@outlook.com>
2024-06-03 21:55:51 +0800

    fix offchain taints-2

--- tests/evm/taints-2/test.sol
+++ tests/evm/taints-2/test.sol
@@ -5,10 +5,11 @@ import "../../../solidity_utils/lib.sol";
 
 contract sb {
     bytes32 answer = 0x60298f78cc0b47170ba79c10aa3851d7648bd96f2f8e46a19dbc777c36fb0c00;
+
     function lol(bytes32 x) public {
         require(msg.sender == address(0x0A101a8A56121470c49B5f477CE7eE7376a92c0a));
         require(x == answer);
-        require(x != answer);
+        // require(x != answer);
         typed_bug("0x3322");
     }
 }
