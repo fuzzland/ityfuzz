1473039fd9b6c4f3ebdf1aab47137e191e772935
Jacob Chia <jacob-chia@outlook.com>
2024-08-01 15:20:02 +0800

    support arbitrum

--- src/evm/onchain/endpoints.rs
+++ src/evm/onchain/endpoints.rs
@@ -1124,7 +1124,7 @@ impl OnChainConfig {
         } else {
             format!("https://pairs-all.infra.fuzz.land/pairs/{network}/{token}")
         };
-        info!("🔥 {url}");
+        // info!("{url}");
         let resp: Value = reqwest::blocking::get(url).unwrap().json().unwrap();
         let mut pairs: Vec<PairData> = Vec::new();
         if let Some(resp_pairs) = resp.as_array() {
