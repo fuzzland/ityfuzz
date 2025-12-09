89144e07548d261c88692feab13dea76130aa11d
Jacob <iamjacobjia@gmail.com>
2025-12-09 12:51:39 +0800

    disable foundry support

--- src/evm/middlewares/mod.rs
+++ src/evm/middlewares/mod.rs
@@ -1,5 +1,6 @@
 pub mod call_printer;
-pub mod cheatcode;
+// TODO: enable cheatcode when https://github.com/matter-labs/zksync-era/issues/4581 is resolved
+// pub mod cheatcode;
 pub mod coverage;
 pub mod middleware;
 pub mod reentrancy;
